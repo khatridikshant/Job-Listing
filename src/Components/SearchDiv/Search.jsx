1ae756ce14b5314b643e1699a313529ddd86cea8
Dikshant Khatri <khatridikshant@outlook.com>
2024-08-11 13:14:22 +0545

    Search Box More Added

--- src/Components/SearchDiv/Search.jsx
+++ src/Components/SearchDiv/Search.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineClockCircle, AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineEnvironment } from "react-icons/ai";
 
 const Search = () => {
   return (
@@ -7,11 +10,26 @@ const Search = () => {
       <form action=''>
         <div className='bg-slate-50 flex justify-between items-center p-5 shadow-lg'>
           <div className='flex flex-grow items-center gap-2'>
-          <AiOutlineSearch className='text-lg icon'/>
-          <input className='focus:outline-none bg-transparent w-full p-1'></input>
+          <AiOutlineSearch className='text-2xl icon'/>
+          <input className='focus:outline-none bg-transparent  p-1' placeholder='Search By Jobs'></input>
+          <AiOutlineCloseCircle className='text-2xl icon' />
           </div>
-
+          <div className='flex flex-grow items-center gap-2'>
+          <AiOutlineHome className='text-2xl icon'/>
+          <input className='focus:outline-none bg-transparent  p-1' placeholder='Search By Home'></input>
+          <AiOutlineCloseCircle className='text-2xl icon' />
+          </div>
+          <div className='flex flex-grow items-center gap-2'>
+          <AiOutlineEnvironment className='text-2xl icon'/>
+          <input className='focus:outline-none bg-transparent  p-1' placeholder='Search By Location'></input>
+          <AiOutlineCloseCircle className='text-2xl icon' />
+          </div>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Search
+</button>
         </div>
+
+        
 
       </form>
     </div>
