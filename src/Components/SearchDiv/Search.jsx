39bee2d8e7b0b21069b0920b194c4a0b3d0ef1f8
Dikshant Khatri <khatridikshant@outlook.com>
2024-08-11 12:56:53 +0545

    Search Box Looks Proper

--- src/Components/SearchDiv/Search.jsx
+++ src/Components/SearchDiv/Search.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { AiOutlineSearch } from "react-icons/ai";
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='searchDiv grid gap-10 bg-gray-200 rounded p-[3rem] mt-2'>
+      <form action=''>
+        <div className='bg-slate-50 flex justify-between items-center p-5 shadow-lg'>
+          <div className='flex flex-grow items-center gap-2'>
+          <AiOutlineSearch className='text-lg icon'/>
+          <input className='focus:outline-none bg-transparent w-full p-1'></input>
+          </div>
+
+        </div>
+
+      </form>
+    </div>
   )
 }
 
