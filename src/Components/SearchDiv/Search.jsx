cd1b10401efa2afda5fadc0df83fa479a2753605
Dikshant Khatri <khatridikshant@outlook.com>
2024-08-11 14:11:01 +0545

    Sorting Design Completed

--- src/Components/SearchDiv/Search.jsx
+++ src/Components/SearchDiv/Search.jsx
@@ -10,28 +10,66 @@ const Search = () => {
       <form action=''>
         <div className='bg-slate-50 flex justify-between items-center p-5 shadow-lg'>
           <div className='flex flex-grow items-center gap-2'>
-          <AiOutlineSearch className='text-2xl icon'/>
-          <input className='focus:outline-none bg-transparent  p-1' placeholder='Search By Jobs'></input>
-          <AiOutlineCloseCircle className='text-2xl icon' />
+            <AiOutlineSearch className='text-2xl icon' />
+            <input className='focus:outline-none bg-transparent  p-1' placeholder='Search By Jobs'></input>
+            <AiOutlineCloseCircle className='text-2xl icon' />
           </div>
           <div className='flex flex-grow items-center gap-2'>
-          <AiOutlineHome className='text-2xl icon'/>
-          <input className='focus:outline-none bg-transparent  p-1' placeholder='Search By Home'></input>
-          <AiOutlineCloseCircle className='text-2xl icon' />
+            <AiOutlineHome className='text-2xl icon' />
+            <input className='focus:outline-none bg-transparent  p-1' placeholder='Search By Home'></input>
+            <AiOutlineCloseCircle className='text-2xl icon' />
           </div>
           <div className='flex flex-grow items-center gap-2'>
-          <AiOutlineEnvironment className='text-2xl icon'/>
-          <input className='focus:outline-none bg-transparent  p-1' placeholder='Search By Location'></input>
-          <AiOutlineCloseCircle className='text-2xl icon' />
+            <AiOutlineEnvironment className='text-2xl icon' />
+            <input className='focus:outline-none bg-transparent  p-1' placeholder='Search By Location'></input>
+            <AiOutlineCloseCircle className='text-2xl icon' />
           </div>
           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-  Search
-</button>
+            Search
+          </button>
         </div>
 
-        
+
 
       </form>
+      <div className='flex items-center mt-1 justify-center gap-2'>
+        <div className='flex m-2'>
+        <div className='flex items-center mr-1'>
+          Sort By:
+        </div>
+        <select id="relevance" className=''>
+          <option>Relevance</option>
+          <option>Inclusive</option>
+          <option>Starts With</option>
+          <option>Contains</option>
+          </select>
+</div>
+<div className='flex m-2'>
+          <div className='flex items-center mr-1'>
+          Type:
+        </div>
+        <select id="type" className=''>
+          <option>Full-Time</option>
+          <option>Remote</option>
+          <option>Part-Time</option>
+          <option>Contract</option>
+          </select>
+          </div>
+
+ <div className='flex m-2'>
+          <div className='flex items-center mr-1'>
+          Type:
+        </div>
+        <select id="level" className=''>
+          <option>Senior</option>
+          <option>Beginner</option>
+          <option>Intermediate</option>
+          <option>Advocate</option>
+          </select>
+          </div>     
+          <span className='icon'> Clear All </span>
+      </div>
+ 
     </div>
   )
 }
