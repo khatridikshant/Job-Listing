da61194c0727ed2de2d652cbf88f2e7453c44f4b
Dikshant Khatri <khatridikshant@outlook.com>
2024-08-12 16:00:40 +0545

    Footer Design

--- src/Components/FooterDiv/Footer.jsx
+++ src/Components/FooterDiv/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
+  return(
+    <>
+    <div className='mt-10 size-[20rem] bg-blue-400 w-full'>
+          <div className='grid grid-rows-10 size-full px-5'>
+        <div></div>
+        <div className='row-span-3 bg-slate-100 px-2 py-2 items-center flex justify-center text-5xl rounded-lg'>Job Finder</div>
+        <div className='row-span-5 rounded px-2 mt-5 py-2 text-white hover:text-black hover:bg-zinc-100'>
+          <p>
+          Our mission is to make job searching a seamless and empowering experience. We are a passionate team of career professionals and technology enthusiasts dedicated to helping individuals find opportunities that align with their skills and ambitions. By offering a user-friendly platform with tailored job recommendations, essential resources, and current industry insights, we aim to equip job seekers with everything they need to succeed. Whether you’re at the beginning of your career, looking to move up, or considering a new direction, we’re here to support you throughout your journey.
+
+
+
+
+          </p>
+        </div>
+          </div>
+    </div>
+    </>
   )
 }
 
